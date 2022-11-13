44ca45bcb40adac08bb5c33eec8a5f89e170f54d
Henrique Doro <opensource@hdoro.dev>
2022-11-13 14:31:45 -0300

    feat: cartoon dots

---
+++ src/routes/cartoon-dots/+page.svelte
@@ -0,0 +1,32 @@
+<div>
+	<img src="https://picsum.photos/seed/lemuel/400" alt="pic" />
+</div>
+
+<style>
+	div {
+		position: relative;
+		display: inline-block;
+	}
+
+	img {
+		display: block;
+	}
+
+	div::after {
+		content: '';
+		display: block;
+		position: absolute;
+		left: 0;
+		top: 0;
+		height: 100%;
+		width: 100%;
+		background-size: 10px 10px;
+		background-image: radial-gradient(
+			circle,
+			rgba(50, 50, 50, 1) 2.5px,
+			rgba(0, 0, 0, 0) 3px
+		);
+		mix-blend-mode: difference;
+		opacity: 0.55;
+	}
+</style>
