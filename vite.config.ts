65f55aedbd0403818df1a8a12ad4d75ebb52ef37
Henrique Doro <opensource@hdoro.dev>
2023-01-03 08:44:27 -0300

    chore: remove HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
-// import mkcert from 'vite-plugin-mkcert'
 
 const config: UserConfig = {
-	plugins: [
-		sveltekit(),
-		//  mkcert()
-	],
+	plugins: [sveltekit()],
 	// Expose over the network to use on mobile
-	server: { host: '0.0.0.0', https: true },
+	server: { host: '0.0.0.0' },
 }
 
 export default config
